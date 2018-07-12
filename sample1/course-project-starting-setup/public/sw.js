4ba4acf895973626944c8eb474d03dae00ecdb39
Rishabh Kalra <rkalra@Avalon>
2018-07-12 12:14:05 +0530

    implemented initial eventlisteners to serviceWorker file

--- sample1/course-project-starting-setup/public/sw.js
+++ sample1/course-project-starting-setup/public/sw.js
@@ -1,1 +1,9 @@
+self.addEventListener('install', function(event){
+    console.log("Install event logged", event);
+});
 
+
+self.addEventListener('activate', function(event){
+    console.log("Activate event logged", event);
+    return self.clients.claim();
+});
