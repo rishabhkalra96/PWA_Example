c6d831f73e0a2214c694877da6f0779c927f1b68
Rishabh Kalra <rkalra@Avalon>
2018-07-12 12:38:59 +0530

    adding non lifecycle events to sw.js

--- sample1/course-project-starting-setup/public/sw.js
+++ sample1/course-project-starting-setup/public/sw.js
@@ -7,3 +7,9 @@ self.addEventListener('activate', function(event){
     console.log("Activate event logged", event);
     return self.clients.claim();
 });
+
+//in order to add event listeners for non lifecycle events like fetch, pudh etc
+
+self.addEventListener('fetch', function(event){
+    console.log("trying to fetch something");
+});
