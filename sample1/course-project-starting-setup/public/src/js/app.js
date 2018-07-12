ba9e785abd66f9667a43f5a016723fe97bba692a
rishabhkalra96 <rishabhkalra96@gmail.com>
2018-07-12 17:57:58 +0530

    levaraging the power of promises

--- sample1/course-project-starting-setup/public/src/js/app.js
+++ sample1/course-project-starting-setup/public/src/js/app.js
@@ -4,6 +4,8 @@ if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js')
     .then(function(){
         console.log('Service worker registered successfully');
+    }).catch(function(err){
+        console.log("Something unexpected happened while registering");
     });
 }
 
