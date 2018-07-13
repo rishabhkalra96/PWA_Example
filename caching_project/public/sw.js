24947b1d789fbf9541dd1b5026c48ea2d763d6d2
rishabhkalra96 <rishabhkalra96@gmail.com>
2018-07-13 17:22:49 +0530

    creating and storing file in cache

--- caching_project/public/sw.js
+++ caching_project/public/sw.js
@@ -1,6 +1,12 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(caches.open('static_cache')
+    .then(function(opened_cache){
+      console.log("cache created/opened");
+      opened_cache.add('/src/js/app.js');
+      console.log("app.js added to cache");
+    }));
 });
 
 self.addEventListener('activate', function(event) {
