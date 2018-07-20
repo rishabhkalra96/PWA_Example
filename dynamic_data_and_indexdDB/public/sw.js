1972a0fc0e57864655091c594d55f57103595cb8
rishabhkalra96 <rishabhkalra96@gmail.com>
2018-07-20 12:15:13 +0530

    activating indexedDB to store dynamic data

--- dynamic_data_and_indexdDB/public/sw.js
+++ dynamic_data_and_indexdDB/public/sw.js
@@ -1,4 +1,6 @@
-var CACHE_STATIC_NAME = 'static-v15';
+importScripts('/src/js/idb.js');
+
+var CACHE_STATIC_NAME = 'static-v17';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -7,6 +9,7 @@ var STATIC_FILES = [
   '/src/js/app.js',
   '/src/js/feed.js',
   '/src/js/promise.js',
+  '/src/js/idb.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
   '/src/css/app.css',
@@ -17,6 +20,12 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
+
+var idb_promise = idb.open('post-store',1, function(db_object){
+  if(!db_object.objectStoreNames.contains('posts')){
+    db_object.createObjectStore('posts',{keyPath: 'id'});
+  }
+});
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
 //     .then(function (cache) {
@@ -70,18 +79,27 @@ function isInArray(string, array) {
 
 self.addEventListener('fetch', function (event) {
 
-  var url = 'https://httpbin.org/get';
+  var url = 'https://pwa-example-5992b.firebaseio.com/posts';
+
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              // trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request, res.clone());
+    event.respondWith(fetch(event.request)
+      .then(function (res) {
+             var cloneRes = res.clone();
+             cloneRes.json()
+             .then(function(data){
+               for(var key in data){
+                  idb_promise
+                  .then(function(db){
+                    var tx = db.transaction('posts', 'readwrite');
+                    var store = tx.objectStore('posts');
+                    console.log("storing" + data[key].Name + "in indexDB");
+                    store.put(data[key]);
+                    return tx.complete;
+                  });
+               }
+             });
               return res;
-            });
-        })
+            })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
     event.respondWith(
